72b5297a2bf3d67ce416442924db3325b542282b
vidhya-maddisetty <vidhya.maddisetty@happiestminds.com>
2016-08-17 06:27:11 +0100

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -4,7 +4,7 @@ int main() {
     int num, fact, i;
 
     printf("Entered number is:5 ");
-    while(scanf("%i", &num)) {
+    
         fact = 1;
         for (i=2; i<=num; i++) {
             fact *= i;
